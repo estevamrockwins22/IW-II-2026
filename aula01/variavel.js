87d28b717d84f89b6d5718fc51af87a4ae5c19bb
Giovanni Estevam <estevam2204@gmail.com>
2026-02-23 08:46:13 -0300

    Add files via upload

---
+++ aula01/variavel.js
@@ -0,0 +1,29 @@
+var nomeDaVariavel = "valor da variável";
+var outraVariavel = 'valor da outra variável';
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+var variavelIndefinida;
+var variavelNula = null;
+
+console.log(nomeDaVariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+console.log(variavelNula);
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
+
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+console.log(int1);
+console.log(int2);
